3af98f0bdf666a1180c331ca87c380a55b217d68
qbamms <111325220+qbamms@users.noreply.github.com>
2022-08-19 14:31:51 +0100

    Update myfile.ts

--- myfile.ts
+++ myfile.ts
@@ -1,1 +1,5 @@
 Live is for the Living, Life is Good and God has Blessed the living
+We learn to live, we live to learn.
+We have to be strong and keep moving
+We definetely gonna achieve our dreams and purpose in life
+Persistency and Consistency is the key
